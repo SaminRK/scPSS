a388cbfad56fc1e0c5f4ee77354878ebc43bd829
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2024-11-22 17:46:10 +0600

    Refactor distance functions

--- scpss/distances.py
+++ scpss/distances.py
@@ -2,15 +2,18 @@ import numpy as np
 from scipy.spatial.distance import cdist
 from scipy.stats import gamma
 from kneed import KneeLocator
+from typing import List
 
 
 def get_kth_nn_distance(distances: np.array, k: int) -> np.array:
+    """Returns the k-th nearest neighbor distances from a distance matrix."""
     return distances[:, k]
 
 
-
-def find_optimal_k(reference_distances: np.array, query_distances: np.array, lower_limit: int, upper_limit: int, p_vals: List[int]) -> int:
-
+def find_optimal_k(reference_distances: np.array, query_distances: np.array, lower_limit: int, upper_limit: int, p_vals: List[float]) -> int:
+    """
+    Finds the optimal k by analyzing outlier ratios for different k values.
+    """
     outlier_ratios_for_k = []
 
     for k in range(lower_limit, upper_limit + 1):
@@ -22,38 +25,42 @@ def find_optimal_k(reference_distances: np.array, query_distances: np.array, low
         thresholds = gamma.ppf(q, a=a_fit, loc=loc_fit, scale=scale_fit)
         outlier_ratios = np.mean(query_kth_distances[:, None] > thresholds, axis=0)
         outlier_ratios_for_k.append(outlier_ratios)
-    
+
     outlier_ratios_for_k = np.array(outlier_ratios_for_k)
     mean_outlier_ratio_for_k = np.mean(outlier_ratios_for_k, axis=1)
     index_of_optimal = np.argmax(mean_outlier_ratio_for_k)
-    optimal_k = index_of_optimal + upper_limit
+    optimal_k = lower_limit + index_of_optimal
 
     return optimal_k
 
 
-def find_optimal_p_val(reference_distances, query_distances, k: int):
-
+def find_optimal_p_val(reference_distances: np.array, query_distances: np.array, k: int) -> float:
+    """
+    Finds the optimal p-value for a specific k.
+    """
     reference_kth_distances = reference_distances[:, k+1]
     query_kth_distances = query_distances[:, k]
-    
-    qs = np.arange(850, 1000, 5) * 0.001
+
     a_fit, loc_fit, scale_fit = gamma.fit(reference_kth_distances)
+    qs = np.arange(850, 1001, 5) * 0.001
     thresholds = gamma.ppf(qs, a=a_fit, loc=loc_fit, scale=scale_fit)
     outlier_ratios = np.mean(query_kth_distances[:, None] > thresholds, axis=0)
 
-    kneedle = KneeLocator(qs, outlier_ratios, curve='concave', direction='increasing')
-    optimal_p = 1 - (kneedle.knee + .005)
+    x_values, y_values = 1 - qs, outlier_ratios
+    kneedle = KneeLocator(x_values, y_values, curve='concave', direction='increasing')
+    optimal_p = kneedle.knee + .005
 
     return optimal_p
 
 
-
 def find_optimal_parameters(reference_adata, query_adata, n_comps_upper_limit: int):
-
+    """
+    Finds the optimal parameters for different numbers of components.
+    """
     optimal_parameters_for_n_comps = {}
-    
+
     obsm_str = 'X_pca_harmony'
-    for n_comps in range(2, n_comps_upper_limit+1):
+    for n_comps in range(2, n_comps_upper_limit + 1):
         X_reference = reference_adata.obsm[obsm_str][:, :n_comps]
         X_query = query_adata.obsm[obsm_str][:, :n_comps]
 
@@ -63,7 +70,7 @@ def find_optimal_parameters(reference_adata, query_adata, n_comps_upper_limit: i
         query_distances = cdist(X_query, X_reference)
         query_distances = np.sort(query_distances, axis=1)
 
-        optimal_k = find_optimal_k(reference_distances, query_distances, 5, 50, [.1, .05, .01])
+        optimal_k = find_optimal_k(reference_distances, query_distances, 5, 50, [0.1, 0.05, 0.01])
         optimal_p = find_optimal_p_val(reference_distances, query_distances, optimal_k)
 
         reference_kth_distances = reference_distances[:, optimal_k+1]
@@ -81,3 +88,6 @@ def find_optimal_parameters(reference_adata, query_adata, n_comps_upper_limit: i
             'outlier_ratio': outlier_ratio
         }
 
+        print(n_comps, optimal_parameters_for_n_comps[n_comps])
+
+    return optimal_parameters_for_n_comps
